0225c4a565c10cb13bddac668a66e5a465924def
TobyBeevers <toby.beevers@entegraps.uk>
2024-01-20 20:55:51 +0000

    Add files via upload

--- Assignment One - Program One (Variable Types with IF).py
+++ Assignment One - Program One (Variable Types with IF).py
@@ -1,39 +1,159 @@
-#Create a print statement to direct the user to slect from the list of choices 1, 2, 3, or 4 etc
-print ("Select a Variable Type from this list to get started:")
-print ("1 for an Integer,")
-print ("2 for a Float,")
-print ("3 for a String,")
-print ("or 4 for a Boolean,")
-print () #added an empty print statement to add a space 
+# /This is a simple Python program to collect a users name before presenting /#
+# /info about Variables. The user can select through choices using Strings and Numbers /#
+# /NB extra *print ()* statments have been used to add white space into the Terminal as there is no UI /#
 
-#Create a variable to store the user's choice, force and integer value
-userChoice = int(input("Enter Your Selection between 1-4: "))
+# Ask the User their Name, collect this into a variable called name
+print () #*
+name = input("What is your name? ")  
+print () #* 
 
-print () #added an empty print statement to add a space 
+# Now ask the user if they would like to know about Words, Numbers or something else
+print("Hello", name, "welcome to my program about Variables in Python.")
+print("What would you like to learn about today?")
+print() #*
 
-#Add the if & elif, print statement to display the user's choice and add else to capture incorrect User Selection
-if userChoice == 1:
-  print ("You have selected Integer,")
-  print ("An Integer is a whole number and can be negative or positive")
-  print ("but can't have a decimal point")
-  print ("EXAMPLE 10")
+# Collect user response in a new variable called userChoice1 with some validation on the String
+userChoice1 = input("Type Words for Words, Numbers for Numbers, or Other for something else: ")
+valid1 = '' # This variable is empty, until the user enters a value. 
+            # If the users entry matches the strings correctly 
+            # it will set to True and end the While loop.
+while valid1 is not True:
+  if userChoice1 == 'Words' or userChoice1 == 'Numbers' or userChoice1 == 'Other':
+      valid1 = True
+      # If the user enters the correct string then the variable will become True, 
+      # if not it will loop through again
+      print() #* 
+  else:
+      print () #* 
+      print (userChoice1, "is not a valid choice.")
+      print () #*
+      userChoice1 = input("Type Words for Words, Numbers for Numbers, or Other for something else: ")
 
-elif userChoice == 2:
-  print ("You have selected Float,")
-  print ("a Float is a number with a decimal point and can be negative or positive")
-  print ("but MUST have a decimal point")
-  print ("EXAMPLE 10.8")
+# These are the User choices, if the user selects "Other" then they will be asked to 
+# enter a new variable called userChoice2 which will enter the Nested If's
+      
+# This is the user selection for Words, it tells the user about using Strings in Python
+if userChoice1 == "Words":
+      print ("You have selected to know more about Words, known as Strings or Text in Python!")
+      print () #*
+      print ("Strings in python are surrounded by either single quotation marks, or double quotation marks.")
+      print ("'Hello' is the same as \"hello\" You can display a string literally with the print() function")
+      print ("A str can be letters of numbers but it's important to understand that numbers")
+      print ("stored in str can not be calculated!")
+      print () #*
 
-elif userChoice == 3:
-  print ("You have selected String,")
-  print ("a String is a series of letters or numbers surrounded by quotation marks,")
-  print ("a number within a String can not be used in a calculation")
-  print ("EXAMPLE 'Hello Word'")
+# This is the user selection for Numbers, it tells the user about numbers in Python
+elif userChoice1 == "Numbers":
+      print ("You have selected to know more about Numbers in Python!")
+      print () #*
+      print ("Numbers can be Integers, Floating Point Numbers, or Complex Numbers")
+      print () #*
+      print (" - Int, or integer, is a whole number, positive or negative, without decimal.")
+      print (" - Float, or \"floating point number\" is a number, positive or negative,")
+      print ("   containing one or more decimals.")
+      print (" - Complex is an advanced number, which take a real number and an imaginary number.")
+      print () #*
 
-elif  userChoice == 4:
-  print ("You have selected Boolean,")
-  print ("a boolean is a true or false value and used to represent Yes or No")
-  print ("EXAMPLE True/False or 0/1")
+# This the user slection for Other, this opens a Nest If with a While Loop.
+# the user is asked to select further from 1,2,3,4 to find out more
+else: 
+      print ("You have selected to know more about something else in Python!")
+      print () #*
+      print ("Enter 1 to understand Sequence Types")
+      print ("Enter 2 to understand Mapping Types")
+      print ("Enter 3 to understand Set Types")
+      print ("Enter 4 to understand Boolean Types")
+      print () #* 
+  
+      # Nested If with a While Loop
+      userChoice2 = input("Enter Your Selection between 1-4: ")
+      valid2 = '' 
+      
+      # While Loop is used to validate the variable userChoice2
+      # isdigit() is a built-in method for strings that returns True if all characters in the string are digits (numeric characters 0 to 9), or False
+      while valid2 is not True:   
+        if userChoice2.isdigit() and 1 <= int(userChoice2) <= 4:
+            valid2 = True
+        else: 
+            print () #*
+            print(userChoice2, "is not a valid choice. Please try again.")
+            print () #*
+            userChoice2 = input("Enter Your Selection between 1-4: ")
+        # These are the user choices if the user has selected Other > 1 or 2 or 3 or 4
+         
+      # This Variable is Casting the data in the Variable userChoice2 to an Interger
+      userChoice2 = int(userChoice2)
+      
+      # This is the user selection for Sequence type 
+      if userChoice2 == 1: 
+          print () #* 
+          print ("Thanks", name, "you selected", userChoice2)
+          print ("So you want to know more about Sequence Types in Python!")
+          print () #*
+          print ("An advanced data type, the sequence data type in Python is used to store")
+          print ("sequential data. The sequence data type in python can be considered")
+          print ("as a container that can store different data.")
+          print () #*
 
-else:
-  print ("You have selected an invalid choice, your selection must be between 1 -4")
+        # This is the user selection for Mapping type     
+      elif  userChoice2 == 2: 
+          print () #* 
+          print ("Thanks", name, "you selected", userChoice2)
+          print ("So you want to know more about Mapping Type in Python!")
+          print () #*
+          print ("An advanced data type, a mapping type is a data type comprised of a collection")
+          print ("of keys and associated values. Python's only built-in mapping type is the")
+          print ("dictionary. Dictionaries implement the associative array abstract data type.")
+          print () #*
+                
+        # This is the user selection for Sets, List & Tuples 
+      elif userChoice2 == 3:
+          print () #* 
+          print ("Thanks", name, "you selected", userChoice2)
+          print ("So you want to know more about Sets Types in Python!")
+          print () #*
+          print ("Sets, Tuples & Lists are all similar in that they are ordered collections of data")
+          print (" - A Set is a collection of unique values stored in a single variable.")
+          print ("   A Set is a collection which is unordered, unchangeable, and unindexed.")
+          print () #*
+          print (" - A Tuple is a collection of values that are ordered.")
+          print ("   A Tuple is a collection which is ordered, unchangeable, and indexed.")
+          print () #*  
+          print (" - A List is a collection of values that are ordered and change.")
+          print ("   A List is a collection which is ordered, changeable, and indexed.")
+          print () #*
+
+        # This is the user selection for Boolean type 
+      elif userChoice2 == 4:
+          print () #* 
+          print ("Thanks", name, "you selected", userChoice2)
+          print ("So you want to know more about Boolean Type in Python!")
+          print () #*
+          print ("In programming you often need to know if an expression is True or False,")
+          print ("You can evaluate any expression in Python, and get either, True or False.")
+          print ("When you compare two values, the expression is evaluated and")
+          print ("Python returns the Boolean answer.")
+          print () #*
+        
+
+# The program will now ask the user if they would like to continue, this sets a Variable to Run or End the program. 
+# If the user enter a value that is not Yes or No (and see or statements), the While Loop will continue until the user enters the correct value.
+          
+# If the User enters YES the Variable is not used and the Program will use the exec and run again 
+# If the user enters NO the variable runEnd is set to True and will use exit
+          
+runEnd = ''
+while runEnd is not True:
+  runEnd = input("Would you like to run this program again? ")
+  if runEnd == "No" or runEnd == "no" or runEnd == "N" or runEnd == "n":
+    runEnd = True
+    print ("Thanks for using my Program")
+    exit ()
+  elif runEnd == "Yes" or runEnd == "yes" or runEnd == "Y" or runEnd == "y":
+    print ("Lets Go again")
+    exec(open("Assignment One - Program One (Variable Types with IF).py").read())
+    # The exec starment will need to contain the file name with .py as the extention
+  else:
+    print () #*
+    print(runEnd, "is not a valid choice. Please try again.")
+    runEnd = '' # This empty's the Variable to NULL so the While Loop will run again
--- Assignment One - Program Two (Variable Types with IF AND).py
+++ Assignment One - Program Two (Variable Types with IF AND).py
@@ -1,86 +1,113 @@
-#import the DataTime Library and set a variable with todays date
+# /This is simple Python program that asks a user for the Date of Birth /#
+# /The DoB is then used for various calculation to present back interesting facts /#
+# /NB extra *print ()* statments have been used to add white space into the Terminal as there is no UI /#
+
+# Begin by importing the Python DataTime Library, then set a variable with todays date
 import datetime
 dateNow = datetime.datetime.now()
 
-# I did get some help on this error function from the Devs at work, def is used to define a function
-# The funtion is then called further down the code
-# https://www.w3schools.com/python/python_functions.asp
 def handle_invalid_date_input():
-    print ()
+    print()
     print("Error: Invalid date format. Please enter your Date of Birth in the format DD-MM-YYYY.")
 
-# Create a Program that will capture a Users Age and return information about Key Events
-# Ask the User to enter their DOB
-print ("Would you like to know some interesting facts about your age?")
-print () # Print a blank line
-userAge = str(input("Enter your Date of Birth DD-MM-YYYY: ")) # Capture the Users DOB in this variable
-
-# I did get some help on this error function from the Devs at work
-# The try and except functions are used to try and catch errors
+# I did get some help on this error function from the Devs at work, however I did write 
+# this code myself they just guided me as I wanted something that I hadn't used before
+# def is used to define a function, the funtion is then called further down the code
+# https://www.w3schools.com/python/python_functions.asp
 # https://www.w3schools.com/python/python_try_except.asp#:~:text=The%20try%20block%20lets%20you,when%20there%20is%20no%20error.
-try:
-    parsedDate = datetime.datetime.strptime(userAge, "%d-%m-%Y").date()
-except ValueError:
-    handle_invalid_date_input()
-    exit()
 
-
-# This variable stores the date after is has been converted into a usable date format using the datatime library > strptime function
-# https://docs.python.org/3/library/datetime.html?highlight=datetime#module-datetime
-parsedDate = datetime.datetime.strptime(userAge, "%d-%m-%Y").date()
-#print (parsedDate) #Keep as debug test to see if the date is parsed correctly
-
-#now start returning values
-print () # Print a blank line
-print ("So you are: ")
-
-#Variable to store the age of the user in years
-userYearsOld = dateNow.year - parsedDate.year # Calculate the Users Age in Years
-print ("   * " +str(userYearsOld) + " Years Old")
-
-#Variable to store the age of the user in days
-numberOfDaysOld = (dateNow.date() - parsedDate).days # Calculate the Users Age in Days
-userDaysOld = numberOfDaysOld - dateNow.day # Calculate the Users Age in Days
-print ("   * That's " + str(userDaysOld) + " Days Old")
-
-#Add some calc to use in if & and statements
-if userYearsOld >= 18:
-  print () # Print a blank line
-  print ("And")
-  print ("   * It looks like your old enough to vote!")
-  print ("   * You can grab a beer at the bar!")
-  print ("   * Plus you can also rent a car!")
-
-elif userYearsOld < 18:
-  userAgeCalc = 18 - userYearsOld
-  print () # Print a blank line
-  print ("Not quite there, only " + str(userAgeCalc) + " years until you can vote & grab a beer!")
-
-#Add some trivia statements
-print () # Print a blank line
-print ("Did you know:")
-print ("   * Your age in dog years is over " + str(userYearsOld * 7))
-print ("   * Your age in cat years is over " + str(userYearsOld * 4))
-print ("   * You probably had somewhere near " + str(round(userDaysOld / 7)) + " Sunday Lunches!")
-
-#Add calcs about Driving ages if & and statements
-print () # Print a blank line
-print () # Print a blank line
-print ("What about Driving:")
-if userYearsOld < 16:
-  print ("    * Sorry you can't start Driving just yet!")
+# Now created another function for the main program
+def main():
+    # Ask the User to enter their DOB, collect this into a Variable called userAge
+    print() #*
+    print("Would you like to know some interesting facts about your age?")
+    print() #*
+    userAge = str(input("Enter your Date of Birth DD-MM-YYYY: ")) 
     
-elif userYearsOld >= 16 and userYearsOld <= 70:
-  print ("    * Yep, you're good with driving as long as you've past your test!")
+    # This function uses the Variable parseDate and collects the data from the Variable 
+    # userAge before checking the date format is DD-MM-YYYY
+    # This code is attempting to parse a user's date in the format "DD-MM-YYYY" using the 
+    # strptime method from the datetime library. The strptime method stands for "string parse time" 
+    # and is used to convert a string representation of a date into a datetime object.
 
-elif userYearsOld > 70:
-  print ("    * Did you know you need to be retesting every three years to keep you Licence?")
+    try:
+        parsedDate = datetime.datetime.strptime(userAge, "%d-%m-%Y").date()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        handle_invalid_date_input()
+        print() #*
+        userAge = str(input("Enter your Date of Birth DD-MM-YYYY: "))
+        return
 
+    # Check for future dates
+    if parsedDate > dateNow.date():
+        print("You entered a future date. Please enter a valid Date of Birth that is not in the future.")
+        print() #*
+        return
+            
+    # This variable stores the date after it has been converted into a usable date format 
+    # using the datatime library > strptime function
+    # https://docs.python.org/3/library/datetime.html?highlight=datetime#module-datetime
+    # print (parsedDate) # Keep as debug test to see if the date format is parsed correctly
+    
+    # Now start returning values
+    print() #*
+    print("So you are: ")
+    
+    # Variable to store the age of the user in years
+    userYearsOld = dateNow.year - parsedDate.year # Calculate the Users Age in Years
+    print("   * " +str(userYearsOld) + " Years Old")
+    
+    # Variable to store the age of the user in days
+    numberOfDaysOld = (dateNow.date() - parsedDate).days # Calculate the Users Age in Days
+    userDaysOld = numberOfDaysOld - dateNow.day # Calculate the Users Age in Days
+    print("   * That's " + str(userDaysOld) + " Days Old")
+    
+    # Add some calc to use if & and statements
+    if userYearsOld >= 18:
+      print() #*
+      print("And")
+      print("   * It looks like your old enough to vote!")
+      print("   * You can grab a beer at the bar!")
+      print("   * Plus you can also rent a car!")
+    
+    else:
+      userAgeCalc = 18 - userYearsOld
+      print() #*
+      print("Not quite there, only " + str(userAgeCalc) + " years until you can vote & grab a beer!")
+    
+    #Add some trivia statements
+    print() #*
+    print("Did you know:")
+    print("   * Your age in dog years is over " + str(userYearsOld * 7))
+    print("   * Your age in cat years is over " + str(userYearsOld * 4))
+    print("   * You probably had something like " + str(round(userDaysOld / 7)) + " Sunday Lunches!")
+    
+    #Add calcs about Driving ages if & and statements
+    print() #*
+    print() #*
+    print("What about Driving:")
+    if userYearsOld < 16:
+      print("    * Sorry you can't start Driving just yet!")
+      print() #*
+    
+    elif 16 <= userYearsOld <= 70:
+      print("    * Yep, you're good with driving as long as you've past your test!")
+      print() #*
+    
+    else:
+      print("    * Did you know you need to be retesting every three years to keep you Licence?")
+      print() #*
 
-exit ()
-
-
-
-
-
-
+# This is a While Loop to ask the user if they want to run the program again
+# This checks if the Python script is being run as the main program and not imported as a module. 
+#When a Python script is executed, the interpreter assigns the special variable __name__ the value "__main__" if it is the main program.
+if __name__ == "__main__":
+  while True:
+      main()
+      
+      runEnd = input("Would you like to run this program again? ").lower()
+      if runEnd not in {"yes", "y"}:
+          print()
+          print("Thanks for using my Program")
+          break
